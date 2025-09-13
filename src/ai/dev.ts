c9188b293fcefcc13022b762382f559d7c3f34c8
shonalpas <147411186+shonalpas@users.noreply.github.com>
2025-09-13 22:45:45 +0800

    Add files via upload

---
+++ src/ai/dev.ts
@@ -0,0 +1,9 @@
+
+import { config } from 'dotenv';
+config();
+
+import '@/ai/flows/themed-product-suggestions.ts';
+import '@/ai/flows/summarize-page-flow.ts';
+import '@/ai/flows/eco-impact-flow.ts';
+import '@/ai/flows/get-product-insights-flow.ts';
+
